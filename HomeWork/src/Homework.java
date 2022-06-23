c9f9b872edfe4ad8a356c5659e1dd2218d80e83a
wudlle <misminasyan@icloud.com>
2022-06-24 00:59:39 +0400

    Homework_1

---
+++ HomeWork/src/Homework.java
@@ -0,0 +1,386 @@
+public class Homework {
+    public static void main(String[] args) {
+//        double a,b,c,y;
+//        a = 11;
+//        b = 42;
+//        c = 543;
+//        y = 1;
+//        boolean bb = false;
+        //21
+
+//        if (a > b && a > c) {
+//            System.out.println(a);
+//        } else if (b > a && b > c) {
+//            System.out.println(b);
+//        }else {
+//            System.out.println(c);
+//        }
+
+        //22
+
+//        if (a < b && a < c) {
+//            System.out.println(a);
+//        } else if (b < a && b < c) {
+//            System.out.println(b);
+//        }else {
+//            System.out.println(c);
+//        }
+
+        //23
+//
+//        if (a == 1 || b == 1 || c == 1){
+//            bb = true;
+//            System.out.println(bb);
+//        }else {
+//            System.out.println(bb);
+//        }
+
+        //24
+
+//        if (a == 2 && b == 2){
+//            bb = true;
+//            System.out.println(bb);
+//        } else if (b == 2 && c == 2) {
+//            bb = true;
+//            System.out.println(bb);
+//        } else if (a == 2 && b == 2) {
+//            bb = true;
+//            System.out.println(bb);
+//        }else {
+//            System.out.println(bb);
+//        }
+
+        //25
+//
+//        if ((a + b) > c || (b + c) > a || (a + c) > b ){
+//            System.out.println(y);
+//        } else {
+//            y = 2;
+//            System.out.println(y);
+//        }
+
+        //26
+
+//        if ((a % 2) == 0 || (b % 2) == 0 || (c % 2) == 0) {
+//            System.out.println(y);
+//        } else {
+//            y = 2;
+//            System.out.println(y);
+//        }
+
+        //27
+
+//        if ((a - b) == (b - c) || (a - c) == (c - b) || (b - a) == (a - c) || (b - c) == (c - a) || (c - a) == (a - b) || (c - b) == (b - a)){
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+        //28
+
+//        if ((a / b) == (b / c) || (a / c) == (c / b) || (b / a) == (a / c) || (b / c) == (c / a) || (c / a) == (a / b) || (c / b) == (b / a)){
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+        //29 abc
+
+
+//        if (a < b && b < c){
+//            System.out.println(a + " " + b + " " + c);
+//        } else if (b < a && a < c) {
+//            System.out.println(b + " " + a + " " + c);
+//        } else if (c < b && b < a) {
+//            System.out.println(c + " " + b + " " + a);
+//        } else if (a < c && c < b) {
+//            System.out.println(a + " " + c + " " + b);
+//        } else if (b < c && c < a) {
+//            System.out.println(b + " " + c + " " + a);
+//        } else if (c < b && b < a) {
+//            System.out.println(c + " " + b + " " + a);
+//        } else {
+//            System.out.println(c + " " + a + " " + b);
+//        }
+
+
+        //30
+
+//        if (a > b && b > c){
+//            System.out.println(a + " " + b + " " + c);
+//        } else if (b > a && a > c) {
+//            System.out.println(b + " " + a + " " + c);
+//        } else if (c > b && b > a) {
+//            System.out.println(c + " " + b + " " + a);
+//        } else if (a > c && c > b) {
+//            System.out.println(a + " " + c + " " + b);
+//        } else if (b > c && c > a) {
+//            System.out.println(b + " " + c + " " + a);
+//        } else if (c > b && b > a) {
+//            System.out.println(c + " " + b + " " + a);
+//        } else {
+//            System.out.println(c + " " + a + " " + b);
+//        }
+
+
+        double a, b, c, d, y;
+        a = 16;
+        b = 31;
+        c = 41;
+        d = 8;
+        y = 1;
+        boolean bb = false;
+
+
+        //31
+
+//
+//        if (a > b && a > c && a > d) {
+//            System.out.println(a);
+//        } else if (b > a && b > c && b > d) {
+//            System.out.println(b);
+//        } else if (c > a && c > b && c > d ) {
+//            System.out.println(c);
+//        } else {
+//            System.out.println(d);
+//        }
+
+
+        // 32
+
+
+//        if (a < b && a < c && a < d) {
+//            System.out.println(a);
+//        } else if (b < a && b < c && b < d) {
+//            System.out.println(b);
+//        } else if (c < a && c < b && c < d ) {
+//            System.out.println(c);
+//        } else {
+//            System.out.println(d);
+//        }
+
+
+        //  33
+
+
+//        if (a == 1 || b == 1 || c == 1 || d == 1){
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+
+        // 34
+
+//        if (a + b == c + d || a + c == b + d || a + d == b + c) {
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+
+        //35
+
+
+//        if (a == b + c + d || b == a + c + d || c == a + b + d || d == a + b + c) {
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+
+        //36
+
+//        if (a % 2 == 1 && b % 2 == 1) {
+//            System.out.println(y);
+//        } else if (a % 2 == 1 && c % 2 == 1) {
+//            System.out.println(y);
+//        } else if (a % 2 == 1 && d % 2 == 1 ) {
+//            System.out.println(y);
+//        } else if (b % 2 == 1 && c % 2 == 1) {
+//            System.out.println(y);
+//        } else if (b % 2 == 1 && d % 2 == 1) {
+//            System.out.println(y);
+//        } else if (c % 2 == 1 && d % 2 == 1) {
+//            System.out.println(y);
+//        } else {
+//            y = 2;
+//            System.out.println(y);
+//        }
+
+
+        //37
+
+
+//         if (a - b == b - c && a - b == c - d || a - b == b - d && a - b == d - c || a - c == c - d && a - c == d - b ||
+//        a - c == c - b && a - c == b - d ||
+//        a - d == d - b && a - d == b - c ||
+//        a - d == d - c && a - d == c - b ||
+//        b - a == a - c && b - a == c - d ||
+//        b - a == a - d && b - a == d - c ||
+//        b - c == c - a && b - c == a - d ||
+//        b - c == c - d && b - c == d - a ||
+//        b - d == d - c && b - d == c - a ||
+//        b - d == d - a && b - d == a - c ||
+//        c - a == a - b && c - a == b - d ||
+//        c - a == a - d && c - a == d - b ||
+//        c - b == b - a && c - b == a - d ||
+//        c - b == b - d && c - b == d - a ||
+//        c - d == d - a && c - d == a - b ||
+//        c - d == d - b && c - d == b - a ||
+//        d - a == a - b && d - a == b - c ||
+//        d - a == a - c && d - a == c - b ||
+//        d - b == b - a && d - b == a - c ||
+//        d - b == b - c && d - b == c - a ||
+//        d - c == c - a && d - c == a - b ||
+//        d - c == c - b && d - c == b - a){
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+
+        // 38
+
+//        if (a / b == b / c && a / b == c / d || a / b == b / d && a / b == d / c || a / c == c / d && a / c == d / b ||
+//                a / c == c / b && a / c == b / d ||
+//                a / d == d / b && a / d == b / c ||
+//                a / d == d / c && a / d == c / b ||
+//                b / a == a / c && b / a == c / d ||
+//                b / a == a / d && b / a == d /c ||
+//                b / c == c / a && b / c == a / d ||
+//                b / c == c / d && b / c == d / a ||
+//                b / d == d / c && b / d == c / a ||
+//                b / d == d / a && b / d == a / c ||
+//                c / a == a / b && c / a == b / d ||
+//                c / a == a / d && c / a == d / b ||
+//                c / b == b / a && c / b == a / d ||
+//                c / b == b / d && c / b == d / a ||
+//                c / d == d / a && c / d == a / b ||
+//                c / d == d / b && c / d == b / a ||
+//                d / a == a / b && d / a == b / c ||
+//                d / a == a / c && d / a == c / b ||
+//                d / b == b / a && d / b == a / c ||
+//                d / b == b / c && d / b == c / a ||
+//                d / c == c / a && d / c == a / b ||
+//                d / c == c / b && d / c == b / a){
+//            bb = true;
+//            System.out.println(bb);
+//        } else {
+//            System.out.println(bb);
+//        }
+
+
+
+        //39
+
+
+//        if (a < b && a < c && c < d) {
+//            System.out.println(a + " " + b + " " + c + " " + d);
+//        } else if (a < b && b < d && d < c) {
+//            System.out.println(a + " " + b + " " + d + " " + c);
+//        } else if (a < c && c < b && b < d) {
+//            System.out.println(a + " " + c + " " + b + " " + d);
+//        } else if (a < c && c < d && d < b) {
+//            System.out.println(a + " " + c + " " + d + " " + b);
+//        } else if (a < d && d < c && c < b) {
+//            System.out.println(a + " " + d + " " + c + " " + b);
+//        } else if (a < d && d < b && b < c) {
+//            System.out.println(a + " " + d + " " + b + " " + c);
+//        } else if (b < a && a < c && c < d) {
+//            System.out.println(b + " " + a + " " + c + " " + d);
+//        } else if (b < a && a < d && d < c) {
+//            System.out.println(b + " " + a + " " + d + " " + c);
+//        } else if (b < c && c < a && a < d) {
+//            System.out.println(b + " " + c + " " + a + " " + d);
+//        } else if (b < c && c < d && d < a) {
+//            System.out.println(b + " " + c + " " + d + " " + a);
+//        } else if (b < d && d < a && a < c) {
+//            System.out.println(b + " " + d + " " + a + " " + c);
+//        } else if (b < d && d < c && c < a) {
+//            System.out.println(b + " " + d + " " + c + " " + a);
+//        } else if (c < a && a < b && b < d) {
+//            System.out.println(c + " " + a + " " + b + " " + d);
+//        } else if (c < a && a < d && b < b) {
+//            System.out.println(c + " " + a + " " + d + " " + b);
+//        } else if (c < b && b < a && a < d) {
+//            System.out.println(c + " " + b + " " + a + " " + d);
+//        } else if (c < b && b < d && a < a) {
+//            System.out.println(c + " " + b + " " + d + " " + a);
+//        } else if (c < d && d < a && a < b) {
+//            System.out.println(c + " " + d + " " + a + " " + b);
+//        } else if (c < d && d < b && a < a) {
+//            System.out.println(c + " " + d + " " + b + " " + a);
+//        } else if (d < a && a < b && b < c) {
+//            System.out.println(d + " " + a + " " + b + " " + c);
+//        } else if (d < a && a < c && b < b) {
+//            System.out.println(d + " " + a + " " + c + " " + b);
+//        } else if (d < b && b < a && a < c) {
+//            System.out.println(d + " " + b + " " + a + " " + c);
+//        } else if (d < b && b < c && a < a) {
+//            System.out.println(d + " " + b + " " + c + " " + a);
+//        } else if (d < c && c < a && a < b) {
+//            System.out.println(d + " " + c + " " + a + " " + b);
+//        } else {
+//            System.out.println(d + " " + c + " " + b + " " + a);
+//        }
+
+        //40
+
+//        if (a > b && a > c && c > d) {
+//            System.out.println(a + " " + b + " " + c + " " + d);
+//        } else if (a > b && b > d && d > c) {
+//            System.out.println(a + " " + b + " " + d + " " + c);
+//        } else if (a > c && c > b && b > d) {
+//            System.out.println(a + " " + c + " " + b + " " + d);
+//        } else if (a > c && c > d && d > b) {
+//            System.out.println(a + " " + c + " " + d + " " + b);
+//        } else if (a > d && d > c && c > b) {
+//            System.out.println(a + " " + d + " " + c + " " + b);
+//        } else if (a > d && d > b && b > c) {
+//            System.out.println(a + " " + d + " " + b + " " + c);
+//        } else if (b > a && a > c && c > d) {
+//            System.out.println(b + " " + a + " " + c + " " + d);
+//        } else if (b > a && a > d && d > c) {
+//            System.out.println(b + " " + a + " " + d + " " + c);
+//        } else if (b > c && c > a && a > d) {
+//            System.out.println(b + " " + c + " " + a + " " + d);
+//        } else if (b > c && c > d && d > a) {
+//            System.out.println(b + " " + c + " " + d + " " + a);
+//        } else if (b > d && d > a && a > c) {
+//            System.out.println(b + " " + d + " " + a + " " + c);
+//        } else if (b > d && d > c && c > a) {
+//            System.out.println(b + " " + d + " " + c + " " + a);
+//        } else if (c > a && a > b && b > d) {
+//            System.out.println(c + " " + a + " " + b + " " + d);
+//        } else if (c > a && a > d && b > b) {
+//            System.out.println(c + " " + a + " " + d + " " + b);
+//        } else if (c > b && b > a && a > d) {
+//            System.out.println(c + " " + b + " " + a + " " + d);
+//        } else if (c > b && b > d && a > a) {
+//            System.out.println(c + " " + b + " " + d + " " + a);
+//        } else if (c > d && d > a && a > b) {
+//            System.out.println(c + " " + d + " " + a + " " + b);
+//        } else if (c > d && d > b && a > a) {
+//            System.out.println(c + " " + d + " " + b + " " + a);
+//        } else if (d > a && a > b && b > c) {
+//            System.out.println(d + " " + a + " " + b + " " + c);
+//        } else if (d > a && a > c && b > b) {
+//            System.out.println(d + " " + a + " " + c + " " + b);
+//        } else if (d > b && b > a && a > c) {
+//            System.out.println(d + " " + b + " " + a + " " + c);
+//        } else if (d > b && b > c && a > a) {
+//            System.out.println(d + " " + b + " " + c + " " + a);
+//        } else if (d > c && c > a && a > b) {
+//            System.out.println(d + " " + c + " " + a + " " + b);
+//        } else {
+//            System.out.println(d + " " + c + " " + b + " " + a);
+//        }
+    }
+}
