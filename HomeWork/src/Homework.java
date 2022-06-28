a3de7353b188f295e13f47dacdf3647ce5c5fff6
wudlle <misminasyan@icloud.com>
2022-06-28 14:48:34 +0400

    homework_3

--- HomeWork/src/Homework.java
+++ HomeWork/src/Homework.java
@@ -1,87 +1,238 @@
 public class Homework {
     public static void main(String[] args) {
-        // Task 1
+        int[] a = {32,43,64,21,3,45};
+        int[] b = {5,5,1,3,3};
+        double[] c = {32.1, 43.1, 45.3, 4.3};
+        double[] d = {42.1, 65.3, 42.1, 76};
+        //Task 1 minimum, maximum
 
-
-//        int[] array = new int[5];
-//        for (int i = 0; i < array.length; i++) {
-//            array[i] = 2;
-//        }
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.print(array[i] + " ");
-//        }
-
-
-        // Task 2
-
-
-//        int[] array = new int[100];
-//
-//        for (int i = 1; i < 101; i++) {
-//            array[i-1] = i;
-//        }
-//
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.print(array[i] + " ");
-//        }
-        
-
-        // Task 3
-
-
-//        int[] array = new int[20];
-//
-//        for (int i = -19,m = 0; i < 20 ; i+=2, m++) {
-//            if (i % 2 != 0) {
-//                array[m] = i;
+        //1) maximum a
+//        int max = a[0];
+//        for (int i = 0; i < a.length; i++) {
+//            if(a[i] > max){
+//                max = a[i];
 //            }
 //        }
+//        System.out.println(max);
 //
-//        for (int i = 0; i < array.length; i++) {
-//            System.out.print(array[i] + " ");
-//        }
 
-
-
-
-        // Task 4
-
-
-//        int[] array = {13,15,20,24,54,60};
-//
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] % 5 == 0){
-//                System.out.println(array[i]);
+//        //2) maximum b
+//        int max = b[0];
+//        for (int i = 0; i < b.length; i++) {
+//            if(b[i] > max){
+//                max = b[i];
 //            }
 //        }
+//        System.out.println(max);
 
-
-        // Task 5
-
-
-//        double[] array = {32 , 523, 21, 321, 42, 1};
-//
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i] <= 467.23 && array[i] >= 24.12){
-//                System.out.println(array[i]);
+//        //3) maximum c
+//        double max = c[0];
+//        for (int i = 0; i < c.length; i++) {
+//            if(c[i] > max){
+//                max = c[i];
 //            }
 //        }
+//        System.out.println(max);
 
 
-        // Task 7
-
-
-//        int n = 32;
-//
-//        if (n < 21 && n > 0) {
-//            System.out.println("input N = " + n);
-//            System.out.println("Output");
-//            for (int i = 1; i <= 10; i++) {
-//                System.out.println(n + " * " + i + " = " + (n * i));
+        //4) maximum d
+//        double max = d[0];
+//        for (int i = 0; i < d.length; i++) {
+//            if(d[i] > max){
+//                max = d[i];
 //            }
-//        }else {
-//            System.out.println("The number does not belong to the crowd");
 //        }
+//        System.out.println(max);
+
+
+        //5) minimum a
+//        int min = a[0];
+//        for (int i = 0; i < a.length; i++) {
+//            if(a[i] < min){
+//                min = a[i];
+//            }
+//        }
+//        System.out.println(min);
+
+
+
+
+        //6) minimum b
+//        int min = b[0];
+//        for (int i = 0; i < b.length; i++) {
+//            if(b[i] < min){
+//                min = b[i];
+//            }
+//        }
+//        System.out.println(min);
+
+
+        //7) minimum c
+//        double min = a[0];
+//        for (int i = 0; i < c.length; i++) {
+//            if(c[i] < min){
+//                min = c[i];
+//            }
+//        }
+//        System.out.println(min);
+
+
+        //8) minimum d
+//        double min = d[0];
+//        for (int i = 0; i < d.length; i++) {
+//            if(d[i] < min){
+//                min = d[i];
+//            }
+//        }
+//        System.out.println(min);
+
+
+        //Task 2 maximum ev index
+//        1) maximum ev maximum elementi index a
+//
+//        int max = a[0];
+//        int index = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            if(a[i] > max){
+//                max = a[i];
+//                index = i;
+//            }
+//        }
+//        System.out.println( "index = " + index + " value = " + max);
+
+
+        //2)maximum ev maximum elementi index c
+
+//        double max = c[0];
+//        int index = 0;
+//        for (int i = 0; i < c.length; i++) {
+//            if(c[i] > max){
+//                max = c[i];
+//                index = i;
+//            }
+//        }
+//        System.out.println( "index = " + index + " value = " + max);
+
+
+//        3) maximum ev verjin maximum element index b
+//        int max = b[0];
+//        int index = 0;
+//        for (int i = 0; i < b.length; i++) {
+//            if(b[i] >= max){
+//                max = b[i];
+//                index = i;
+//            }
+//        }
+//        System.out.println( "last index = " + index + " value = " + max);
+
+
+
+        //Task 3 Gumarel arrayi andamnery
+
+//        1)sum A
+
+//        int sum = 0;
+//
+//        for (int i = 0; i < a.length; i++) {
+//            sum = a[i] + sum;
+//        }
+//
+//        System.out.println(sum);
+//
+//        2)sum b
+
+//        for (int i = 0; i < b.length; i++) {
+//            sum = b[i] + sum;
+//        }
+//
+//        System.out.println(sum);
+
+
+        //3) sum c
+
+//        double sum = 0;
+//        for (int i = 0; i < c.length; i++) {
+//            sum = c[i] + sum;
+//        }
+//
+//        System.out.println(sum);
+
+
+//        Task 4 Bazmapatkel arrayi andamnery
+
+
+
+//        1)sum A
+
+//        int sum = 1;
+//
+//        for (int i = 0; i < a.length; i++) {
+//            sum = a[i] * sum;
+//        }
+//
+//        System.out.println(sum);
+//
+//        2)sum b
+//
+//        for (int i = 0; i < b.length; i++) {
+//            sum = b[i] * sum;
+//        }
+//
+//        System.out.println(sum);
+//
+
+        //3) sum c
+
+//        double sum = 1;
+//        for (int i = 0; i < c.length; i++) {
+//            sum = c[i] * sum;
+//        }
+//
+//        System.out.println(sum);
+
+
+        //Task 5 erkvoryakneri xndir
+
+//        int t = 0;
+//        for (int i = 0; i < b.length; i++) {
+//            t = t ^ b[i];
+//        }
+//        System.out.println(t);
+
+
+
+        //Task 6 nman andamneri qanaky
+
+
+        //1) b
+//        int count5 = 0;
+//        int count3 = 0;
+//
+//        for (int i = 0; i < b.length; i++) {
+//            if (b[i] == 5) {
+//                count5++;
+//            }
+//            if (b[i] == 5) {
+//                count3++;
+//            }
+//
+//        }
+//        System.out.println(count5);
+//        System.out.println(count3);
+
+
+        //2) d
+//
+//        int count = 0;
+//
+//        for (int i = 0; i < d.length; i++) {
+//            if (d[i] == 42.1) {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+
 
     }
 }
